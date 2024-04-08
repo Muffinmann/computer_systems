1655047792f5751c8d1249be4807b368f777f637
conner <conner.zhang.gl@gmail.com>
2024-04-08 14:58:25 +0200

    update notes & practice

--- representing_and_manipulation_information/2_2_integer_rep_main.c
+++ representing_and_manipulation_information/2_2_integer_rep_main.c
@@ -1,17 +1,53 @@
 #include <stdio.h>
 
 void main() {
+  // The format specifiers(directives) %d and %u of function "printf" in C are used
+  // respectively for integer and unsigned integer.
+  // The "printf" function does not make use of any type information but
+  // only prints the value with the given specifier. This is to say, it is
+  // possible to print a value of type int with directive %u and a value of type unsigned
+  // with directive %d.
+  short int si = 0xfffa;     // 1111 1111 1111 1010
+  printf("si = %d \n", si);  // -6
+  printf("si = %u \n", si);  // 4294967290
+
+  int i = 0xfffffffa;      // 1111 1111 1111 1111 1111 1111 1111 1010
+  printf("i = %d \n", i);  // -6
+  printf("i = %u \n", i);  // 4294967290
+
+  short int si2 = 0x7ffa;      // 0111 1111 1111 1010
+  printf("si2 = %d \n", si2);  // 32762
+  printf("si2 = %u \n", si2);  // 32762
+
+  int i2 = 0x7ffffffa;       // 0111 1111 1111 1111 1111 1111 1111 1010
+  printf("i2 = %d \n", i2);  // 2147483642
+  printf("i2 = %u \n", i2);  // 2147483642
+
+  char negative_c = 0xff;
+  char positive_c = 0x7f;
+
+  printf("negative c = %d \n", negative_c);  // -1
+  printf("negative c = %u \n", negative_c);  // 4294967295
+  printf("positive c = %d \n", positive_c);  // 127
+  printf("positive c = %u \n", positive_c);  // 127
+
+  // Through the observation, it seems the printf function would fill the
+  // the bit slots by 0 or 1 according to the sign of given number which
+  // has a type that takes less bytes than integer (in example "short int").
+  // If the given number is positive, then the missing slots are filled with 0,
+  // otherwise with 1.
+
   char c0 = 0x00;  // 0000 0000
   char c1 = 0x7f;  // 0111 1111
   char c2 = 0x80;  // 1000 0000
   char c3 = 0xff;  // 1111 1111
 
   // first bit is the sign bit
-  printf("signed \n");
-  printf("%d \n", c0);  // 0
-  printf("%d \n", c1);  // 127
-  printf("%d \n", c2);  // -128
-  printf("%d \n", c3);  // -1
+  printf("c0 = %d \n", c0);  // 0
+  printf("c1 = %d \n", c1);  // 127
+  printf("c2 = %d \n", c2);  // -128
+  printf("c3 = %d \n", c3);  // -1
+
   // signed number in represented as two's-complement form. It interprets
   // the most significant bit of the word to have negative weight.
   // 0xff -> -1*2^7 + 1*2^6 + 1*2^5 + ... + 1*2^0.
@@ -31,7 +67,7 @@ void main() {
   // be given negative or positive.
   // This is to say, we use [00...0] and [10...0] to represent +0 and -0 respectively.
   // The number "3" can be represented as [0011] and the number "-3" is [1011].
-  // This approach is called "Sign-Magnitude"
+  // This approach is called "Sign-Magnitude".
   //
   // The drawback of the last two approaches is clear, that they have two different encodings
   // of the number 0.
@@ -40,20 +76,19 @@ void main() {
   unsigned char uc1 = 0x7f;  // 0111 1111
   unsigned char uc2 = 0x80;  // 1000 0000
   unsigned char uc3 = 0xff;  // 1111 1111
-  short int i = 0xfffa;      // 1111 1111 1111 1010
-  unsigned short int ui = 0xfffa;
 
-  printf("unsigned \n");
-  printf("%u \n", uc0);  // 0
-  printf("%u \n", uc1);  // 127
-  printf("%d \n", uc2);  // 128
-  printf("%d \n", uc3);  // 255
-  printf("%d \n", i);    // -6
-  printf("%u \n", ui);   // 65530
+  printf("uc0 = %u \n", uc0);  // 0
+  printf("uc1 = %u \n", uc1);  // 127
+  printf("uc2 = %u \n", uc2);  // 128
+  printf("uc3 = %u \n", uc3);  // 255
 
   // By changing from unsigned to singed with two's complement, the weight of the first
   // bit has been changed from 2^(w-1) to -2^(w-1), which leads to a difference of 2^w.
   // For example, for a word size of 16-bit(short int), 65530 becomes -6, 65535 becomes -1.
+  int x = -1;
+  unsigned u = 2147483648;
+  printf("x = %u = %d\n", x, x);  // x = 4294967295 = -1
+  printf("u = %u = %d\n", u, u);  // u = 2147483648 = -2147483648
 
   // Question: How C truncates a value by type conversion
   int v = 0x12345678;
@@ -64,9 +99,4 @@ void main() {
   printf("%x \n", sv);  // 5678
   printf("%x \n", j);   // 5678
   // Answer: Seems the high-order bits are discarded.
-
-  int x = -1;
-  unsigned u = 2147483648;
-  printf("x = %u = %d\n", x, x);
-  printf("u = %u = %d\n", u, u);
 }
--- representing_and_manipulation_information/2_2_integer_rep_practice.c
+++ representing_and_manipulation_information/2_2_integer_rep_practice.c
@@ -1,6 +1,8 @@
+#include <limits.h>
 #include <stdio.h>
 
 void practice_2_18() {
+  printf("---practice 2.18--- \n");
   int a, b, c, d, e, f, g, h, i, j;
   a = 0x1b8;
   b = 0x14;
@@ -24,6 +26,30 @@ void practice_2_18() {
   printf("0x20 = %d \n", j);
 }
 
+void practice_2_21() {
+  printf("---practice 2.21--- \n");
+  printf("INT_MAX = %d\n", INT_MAX);  // 2147483647 #define INT_MAX __INT_MAX__
+  printf("INT_MIN = %d\n", INT_MIN);  // -2147483648 #define INT_MIN (-INT_MAX - 1)
+
+  int max_i = 2147483647;      // 0x7fffffff
+  int min_i = -2147483647;     // 0x80000001
+  int min_i_ng = -2147483648;  // 0x80000000
+
+  printf("%d \n", min_i - 1 == 2147483648);   // 0
+  printf("%d \n", min_i - 1 == 2147483648u);  // 1
+  // when comparing signed and unsigned values, the signed value will
+  // be implicitly converted into unsigned
+
+  printf("%d \n", min_i - 1 < max_i);         // 1
+  printf("%d \n", min_i - 1u < max_i);        // 0; 2147483648 < 2147483647
+  printf("%d \n", -2147483647 - 1u < max_i);  // 0
+
+  printf("%d \n", min_i - 1 < min_i);         // 1
+  printf("%d \n", min_i - 1u < min_i);        // 1; 0x80000000 < 0x80000001
+  printf("%d \n", -2147483647 - 1u < min_i);  // 1
+}
+
 void main() {
   practice_2_18();
+  practice_2_21();
 }
