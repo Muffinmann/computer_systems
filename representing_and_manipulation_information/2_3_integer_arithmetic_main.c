ccce4303a153adda3fd10ec05ab2db3719905b19
conner <conner.zhang.gl@gmail.com>
2024-04-14 17:18:43 +0200

    update

--- representing_and_manipulation_information/2_3_integer_arithmetic_main.c
+++ representing_and_manipulation_information/2_3_integer_arithmetic_main.c
@@ -37,4 +37,18 @@ void main() {
   // For a given word size w, the maximum value in two's complement is 2^(w-1) - 1
   // If the sum is over this value, the overflow would yield a value which is equal
   // to x + y - 2^(w)
+
+  // two's complement negation
+  int x4 = 0x80000000;                    // -2147483648
+  printf("-x = -(%d) = %d \n", x4, -x4);  // -2147483648
+
+  x4 = 0x7fffffff;                        // 2147483647
+  printf("-x = -(%d) = %d \n", x4, -x4);  // -2147483647; 0x80000001
+
+  x4 = 0xfffffffa;                        // -6
+  printf("-x = -(%d) = %d \n", x4, -x4);  // 6; 0x00000006
+  // for x in the range (-2^(w-1), 2^(w-1)], the two's complement negation
+  // is simply -x, for the case x = -2^(w-1), or x =Tmin, then the negation
+  // of x is again -2^(w-1)
+  // In general, -x = ~x + 1
 }
--- representing_and_manipulation_information/2_3_integer_arithmetic_practice.c
+++ representing_and_manipulation_information/2_3_integer_arithmetic_practice.c
@@ -51,22 +51,45 @@ void practice_2_30() {
 int tadd_ok_buggy(int x, int y) {
   int sum = x + y;
   return (sum - x == y) && (sum - y == x);
-  // This check will fail
+  // This check will fail since subtraction will essentially revert the overflow
+  // and leads the condition to always be true.
 }
 void practice_2_31() {
   printf("---practice 2.31---\n");
   int x, y;
   x = 0x80000000;
   y = 0x80000000;
-  printf("x + y = %d + %d ok? %d\n", x, y, tadd_ok_buggy(x, y));
+  printf("x + y = %d + %d = %d ok? %d\n", x, y, x + y, tadd_ok_buggy(x, y));  // 1
 
   x = 0xffffffff;
   y = 0x80000000;
-  printf("x + y = %d + %d ok? %d\n", x, y, tadd_ok_buggy(x, y));
+  printf("x + y = %d + %d = %d ok? %d\n", x, y, x + y, tadd_ok_buggy(x, y));  // 1
+
+  x = 0x00000001;
+  y = 0x7fffffff;
+  printf("x + y = %d + %d =%d ok? %d\n", x, y, x + y, tadd_ok_buggy(x, y));  // 1
+}
+
+// Determine whether arguments can be subtracted (x-y) without overflow
+int tsub_ok(int x, int y) {
+  return tadd_ok(x, -y);
+  // This check will fail if y is equal to -2^(w-1), since in this case
+  // -y is e
+}
+
+void practice_2_32() {
+  printf("---practice 2.32---\n");
+  int x, y;
+  x = 0xffffffff;                                                      // -1
+  y = 0x80000000;                                                      // -2147483648
+  printf("x - y =%d - %d = %d ok? %d\n", x, y, x + y, tsub_ok(x, y));  // ok 0
+  printf("y = %d\n", y);
+  printf("-y = %d\n", -y);
 }
 
 void main() {
   practice_2_27();
   practice_2_30();
   practice_2_31();
+  practice_2_32();
 }
