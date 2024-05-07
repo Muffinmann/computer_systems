466678348d710ed1b42b86ba39614424f51753f5
conner <guanlai.zhang@orfo.de>
2024-05-07 10:49:46 -0500

    add comment

--- representing_and_manipulation_information/2_3_integer_arithmetic_main.c
+++ representing_and_manipulation_information/2_3_integer_arithmetic_main.c
@@ -51,4 +51,33 @@ void main() {
   // is simply -x, for the case x = -2^(w-1), or x =Tmin, then the negation
   // of x is again -2^(w-1)
   // In general, -x = ~x + 1
+
+
+  // tow's complement multiplication
+  short x5, y5;
+  x5 = 0x8000;
+  y5 = 0x8000;
+  // result product: 0x40000000
+  printf("x * y = %hi * %hi = %hi\n", x5, y5, x5 * y5); // x * y = 0 = 0x0000
+
+  x5 = 0x7fff;
+  y5 = 0x7fff;
+  // result product: 0x3fff0001
+  printf("x * y = %hi * %hi = %hi\n", x5, y5, x5 * y5); // x * y = 1 = 0x0001
+
+  x5 = 0x8000;
+  y5 = 0x7fff;
+  // result product: 0xc0008000
+  printf("x * y = %hi * %hi = %hi\n", x5, y5, x5 * y5); // x * y = -32768 = 0x8000
+
+  x5 = 1143;
+  y5 = 1987367;
+  // result product: 0x87654321
+  printf("x * y = %hi * %hi = %hi\n", x5, y5, x5 * y5); // x * y = 17185 = 0x4321
+
+  x5 = 9;
+  y5 = 89;
+  // result product: 0x00000321
+  printf("x * y = %hi * %hi = %hi\n", x5, y5, x5 * y5); // x * y = 801 = 0x321
+  // an overflow in multiplication of 2w bits will be simply truncted to w bits for either signed and unsigned values.
 }
