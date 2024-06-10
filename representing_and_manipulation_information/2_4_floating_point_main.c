547cb07aef944aee7d2ff3f03f86761e3b8b2a95
conner <conner.zhang.gl@gmail.com>
2024-06-10 22:24:57 +0200

    update note

--- representing_and_manipulation_information/2_4_floating_point_main.c
+++ representing_and_manipulation_information/2_4_floating_point_main.c
@@ -13,26 +13,36 @@ void print_byte(byte_pointer start, int len) {
 void print_float_byte(float x) {
   print_byte((byte_pointer)&x, sizeof(float));
 }
+
+void print_int_byte(int x) {
+  print_byte((byte_pointer)&x, sizeof(int));
+}
 void main() {
+  int i;
+  i = +0;
+  print_int_byte(i);  // 00 00 00 00 -> 0x00000000 -> 0000 0000 0000 0000 0...0
+  i = -0;
+  print_int_byte(i);  // 00 00 00 00 -> 0x00000000 -> 0000 0000 0000 0000 0...0
+
   float x;
-  x = 1.0;
-  print_float_byte(x);  // 00 00 80 3f -> 0x3f800000 -> 0011 1111 1000 0000 0...0
-  x = 1.5;
-  print_float_byte(x);  // 00 00 c0 3f -> 0x3fc00000 -> 0011 1111 1100 0000 0...0
-  x = 1.25;
-  print_float_byte(x);  // 00 00 a0 3f -> 0x3fa00000 -> 0011 1111 1010 0000 0...0
-  x = 1.125;
+  x = +0.0;
+  print_float_byte(x);  // 00 00 00 00 -> 0x00000000 -> 0000 0000 0000 0000 0...0
+  x = -0.0;
+  print_float_byte(x);  // 00 00 00 80 -> 0x80000000 -> 1000 0000 0000 0000 0...0
+  // float number has a different bit representation for +0 and -0 with IEEE format.
+
+  x = 0x7f900000;
   print_float_byte(x);  // 00 00 90 3f -> 0x3f900000 -> 0011 1111 1001 0000 0...0
   x = 1.625;
   print_float_byte(x);  // 00 00 d0 3f -> 0x3fd00000 -> 0011 1111 1101 0000 0...0
   x = 1.375;
   print_float_byte(x);  // 00 00 b0 3f -> 0x3fb00000 -> 0011 1111 1011 0000 0...0
-  x = 0.5; // 1/2
+  x = 0.5;              // 1/2
   print_float_byte(x);  // 00 00 00 3f -> 0x3f000000 -> 0011 1111 0000 0000 0...0
-  x = 0.25; // 1/4
+  x = 0.25;             // 1/4
   print_float_byte(x);  // 00 00 80 3e -> 0x3e800000 -> 0011 1110 1000 0000 0...0
-  x = 0.75; // 3/4
+  x = 0.75;             // 3/4
   print_float_byte(x);  // 00 00 40 3f -> 0x3f400000 -> 0011 1111 0100 0000 0...0
- 
+
   //
 }
